9da75f520f2030e7d8c16427cf3e82bf02a3dec1
Flik Jeong <jsh852@gmail.com>
2019-12-02 15:37:53 +0900

    About컴포넌트를 따로 안불러오도록함

--- src/pages/about2.jsx
+++ src/pages/about2.jsx
@@ -1,7 +1,11 @@
+/** src/pages/about2.jsx */
 import React, { Component } from 'react';
+import Card from 'react-md/lib/Cards/Card';
+import CardText from 'react-md/lib/Cards/CardText';
 import Helmet from 'react-helmet';
+import UserLinks from '../components/UserLinks';
+
 import Layout from '../layout';
-import About from '../components/About';
 import config from '../../data/SiteConfig';
 
 class AboutPage2 extends Component {
@@ -13,7 +17,25 @@ class AboutPage2 extends Component {
             <title>{`About2 | ${config.siteTitle}`}</title>
             <link rel='canonical' href={`${config.siteUrl}/about2/`} />
           </Helmet>
-          <About />
+          <div className='about-container md-grid mobile-fix'>
+            <Card className='md-grid md-cell--8'>
+              <div className='about-wrapper'>
+                <img
+                  src={config.userAvatar}
+                  className='about-img'
+                  alt={config.userName}
+                />
+                <CardText>
+                  <p className='about-text md-body-1'>글자글자</p>
+                  <p className='about-text md-body-1'>카드텍스트</p>
+                  <p className='about-text md-body-1'>
+                    {config.userDescription}
+                  </p>
+                </CardText>
+                <UserLinks labeled config={config} />
+              </div>
+            </Card>
+          </div>
         </div>
       </Layout>
     );
