8f8473dc5d4dbcef790dcca5c75bb5b502df7e24
Flik Jeong <jsh852@gmail.com>
2019-12-02 15:38:26 +0900

    잘못쓴 기호 수정

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,7 +18,7 @@ class Index extends React.Component {
             <link rel='canonical' href={`${config.siteUrl}`} />
           </Helmet>
           <SEO postEdges={postEdges} />
-          <PostListing postEdges={postEdges} />;
+          <PostListing postEdges={postEdges} />
         </div>
       </Layout>
     );
