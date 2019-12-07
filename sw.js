b2eab6f796896c32dc641ede030ef5db57e6668b
Flik Jeong <jsh852@gmail.com>
2019-12-08 00:09:37 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4fb93c9166a40f742ede.js"
+    "url": "webpack-runtime-91592e5436be1c671f86.js"
   },
   {
     "url": "app-da85bea47a3956b137fa.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7f3fa19bd05ea81200017045fa7441be"
+    "revision": "5ff81b9017fa40dbf8d57cc90eb8ce37"
   },
   {
     "url": "static/d/604/path---offline-plugin-app-shell-fallback-a-30-c5a-BawJvyh36KKFwbrWPg4a4aYuc8.json"
